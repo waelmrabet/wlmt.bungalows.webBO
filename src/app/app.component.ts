e1dd55e2415162c1fbef745c8506a38af23c4588
unknown <wael.mrabet@esprit.tn>
2023-05-23 18:50:23 +0200

    adapt template to login process

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import * as $ from "jquery";
+import { AccountService } from './services/account.service';
+import { User } from './models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,17 @@ import * as $ from "jquery";
 })
 export class AppComponent {
   title = 'wlmt.bungalows.webBO';
+  public user: User | undefined;
+
+  constructor(private accountService: AccountService, private router: Router,){
+    
+    this.accountService.user.subscribe(currentUser => {
+      if(currentUser)
+        this.user = currentUser;
+      else{
+        this.user = undefined;
+        this.router.navigateByUrl("account/login");
+      }
+    })
+  }
 }
